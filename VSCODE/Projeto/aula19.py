daefe0f3dd25bdf8045e6cb22fc3b5f7bd2d8e3f
evandrotm <evandrogb72@gmail.com>
2025-10-27 23:38:13 -0300

    aula 19

---
+++ VSCODE/Projeto/aula19.py
@@ -0,0 +1,24 @@
+"""
+Operadores de comparação (relacionais)
+Op ...... Significado ....... Exemplo (True)
+> ....... maior que .......... 2 > 1
+>= ...... maior ou igual ..... 2 >= 2       
+< ....... menor que .......... 1 < 2
+<= ...... menor ou igual ..... 2 <= 2   
+== ...... igual ............. 2 == 2
+!= ...... diferente ......... 2 != 1
+"""
+
+maior = 2 > 1
+maior_ou_igual = 2 >= 2 
+menor = 1 < 2
+menor_ou_igual = 2 <= 2
+igual = 2 == 2
+diferente = 2 != 1
+print('Maior:', maior)
+print('Maior ou igual:', maior_ou_igual)
+print('Menor:', menor)
+print('Menor ou igual:', menor_ou_igual)
+print('Igual:', igual)
+print('Diferente:', diferente)  
+print('----------------------------------')
